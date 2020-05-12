f94bec78c822f0d04ca1449dfc67e6186f3cf9e1
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2020-05-13 01:15:32 +0200

    refactorized classes + removed fixed height

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -1,14 +1,30 @@
 let previousScroll = 30;
+let navbar = document.getElementById("navbar");
 
 window.onscroll = function () {
-  scrollFunction();
+  toggleVisibility();
+  toggleColor();
 };
 
-function scrollFunction() {
+function toggleVisibility() {
   if (window.scrollY > previousScroll) {
-    document.getElementById("navbar").style.visibility = "hidden";
+    navbar.style.visibility = "hidden";
   } else {
-    document.getElementById("navbar").style.visibility = "visible";
+    navbar.style.visibility = "visible";
   }
   previousScroll = window.scrollY;
+}
+
+function toggleColor() {
+  let myColoredElements = document.querySelectorAll(".main-color");
+  // console.log(myColoredElements);
+  myColoredElements.forEach((oneColoredElement) => {
+    let currentBgColor = window.getComputedStyle(oneColoredElement)
+      .backgroundColor;
+    let start = oneColoredElement.offsetTop;
+    let end = oneColoredElement.offsetTop + oneColoredElement.offsetHeight;
+    if (window.scrollY > start && window.scrollY < end) {
+      navbar.style.backgroundColor = currentBgColor;
+    }
+  });
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("home", { style: "home" });
+  res.render("home", { styles: ["header", "home", "buttons"] });
+});
+
+router.get("/prestations", function (req, res, next) {
+  res.render("presta", { styles: ["header", "presta", "buttons"] });
 });
 
 module.exports = router;
