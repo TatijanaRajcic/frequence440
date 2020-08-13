456e83dd6de12104a063511f370af6e012dcfd67
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2020-08-13 11:22:23 +0200

    added selected options on contact form

--- public/javascripts/contact.js
+++ public/javascripts/contact.js
@@ -1,6 +1,13 @@
 let contactButton = document.querySelector("#send-contact");
 contactButton.onclick = sendContact;
 
+let prestaDetails = [
+  { presta: "diagnostics", details: ["Diagnostics1", "Diagnostics2"] },
+  { presta: "ateliers", details: ["ATELIER1", "ATELIER2"] },
+  { presta: "conférences", details: ["conf1", "conf2"] },
+  { presta: "formations", details: ["form1", "form2"] },
+];
+
 function sendContact() {
   let contactToSend = {
     email: document.querySelector('input[name="email"]').value,
@@ -61,3 +68,78 @@ function sendInvoice() {
       console.log(error);
     });
 }
+
+function addNewService() {
+  let alreadyExistingServices = document.querySelectorAll(".service").length;
+  let index = alreadyExistingServices + 1;
+  let newService = document.createElement("div");
+  newService.setAttribute("class", "service bordered");
+  newService.innerHTML = ` 
+  <div class="index-container flex space-b">
+    <p class="index">0${index}</p>
+    <img src="/images/menu-close-white.svg" class="close-invoice"></img>
+  </div>
+  <div class="form-group flex-col">
+    <label for="presta-type-${index}">Type (diagnostic, atelier, conférence, formation)</label>
+    <select class="select-presta-type" name="presta-${index}" id="presta-type-${index}">
+      <option value=""></option>
+      <option value="diagnostics">Diagnostics</option>
+      <option value="ateliers">Ateliers</option>
+      <option value="conférences">Conférences</option>
+      <option value="formations">Formations</option>
+    </select>
+  </div>
+  <div class="form-group flex-col">
+    <label for="details-presta-${index}">Nom de la prestation</label>
+    <select class="select-presta-details" name="details-${index}" id="details-presta-${index}">
+      <option value=""></option>
+    </select>
+  </div>
+  <div class="form-group flex-col">
+    <label for="client-quantity-${index}">Nombre de personnes<span class="mandatory">*</span></label>
+    <input type="number" name="quantity-${index}" id="client-quantity-${index}" required />
+  </div>
+  <div class="form-group flex-col">
+    <label for="client-message-${index}">Message (merci de préciser les équipes qui seront concernées, les
+      contraintes de temps / espace / matériel, etc.)</label>
+    <textarea name="message-${index}" id="client-message-${index}" cols="30" rows="6"></textarea>
+  </div>`;
+  newService.querySelector(".close-invoice").onclick = removeService;
+  newService.querySelector(".select-presta-type").onchange = (e) => {
+    fillPrestaList(e);
+  };
+  document.getElementById("services").appendChild(newService);
+}
+
+function removeService(e) {
+  e.target.closest(".service").remove();
+  updateIndexes();
+}
+
+function updateIndexes() {
+  document.querySelectorAll(".service").forEach((oneService, index) => {
+    oneService.querySelector(".index").innerHTML = `0${index + 1}`;
+  });
+}
+
+function fillPrestaList(e) {
+  let selectedType = e.target.options[e.target.selectedIndex].value;
+  let listToFill = e.target
+    .closest(".service")
+    .querySelector(".select-presta-details");
+  listToFill.innerHTML = "";
+  let options = prestaDetails.find((element) => element.presta === selectedType)
+    .details;
+  options.forEach((oneOption) => {
+    let newOption = document.createElement("option");
+    newOption.value = oneOption;
+    newOption.innerHTML = oneOption;
+    listToFill.appendChild(newOption);
+  });
+}
+
+document.getElementById("add").onclick = addNewService;
+document.querySelector(".close-invoice").onclick = removeService;
+document.querySelector(".select-presta-type").onchange = (e) => {
+  fillPrestaList(e);
+};
